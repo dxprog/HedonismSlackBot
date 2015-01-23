70327259882f5f05df6af2309516910c9e674052
Matt Hackmann <matt@dxprog.com>
2015-01-23 15:24:55 -0800

    herpderp

--- plugin/leave.php
+++ plugin/leave.php
@@ -2,17 +2,17 @@
 
 namespace Plugin {
 
-    class Insult implements \SlackPlugin {
+    class Leave implements \SlackPlugin {
 
         private static $_phrases = [
-          '{name} has left the room',
-          '{name} slams the door in @mak\'s face'
+          '*{name} has left the room*',
+          '*{name} slams the door in @mak\'s face*'
         ];
 
         public static function trigger($slack, $params) {
             $request = $slack->getRequest();
             if ($request->user_name) {
-              $phrase = self::$_phrases[rand() % (count(self::$_phrases) - 1)];
+              $phrase = self::$_phrases[rand() % count(self::$_phrases)];
               $slack->respond(str_replace('{name}', $request->user_name, $phrase));
             }
         }
