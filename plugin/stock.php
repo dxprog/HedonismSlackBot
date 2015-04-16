bb0e12517f8c6f2261d788fa94cc56c09264e1f6
Matt Hackmann <matt@dxprog.com>
2015-04-16 09:44:06 -0700

    changed data provider

--- plugin/stock.php
+++ plugin/stock.php
@@ -19,43 +19,67 @@ namespace Plugin {
             $stocks = count($params) ? $params : self::$_defaultStocks;
             $stocks = !is_array($stocks) ? [ $stocks ] : $stocks;
 
-            // Sanitize the array
-            $safeStocks = [];
+            $mh = curl_multi_init();
+
+            // Sanitize the array and start building up requests
+            $requests = [];
             foreach($stocks as $stock) {
                 if (preg_match('/^[A-Za-z]+$/', $stock)) {
-                    $safeStocks[] = $stock;
+                    $request = self::_createStockRequest($stock);
+                    curl_multi_add_handle($mh, $request->dataRequest);
+                    $requests[] = $request;
                 }
             }
 
-            if (count($safeStocks) > 0) {
-                $url = 'http://finance.google.com/finance/info?client=ig&q=' . implode(',', $safeStocks);
-                $data = @file_get_contents($url);
-                if ($data) {
-                    $data = json_decode(substr(trim($data), 2));
-                    if ($data) {
-
-                        $out = [];
-                        $afterHours = [];
-                        foreach ($data as $info) {
-                            $emoji = (float) $info->c > 0 ? ':green_heart:' : ':broken_heart:';
-                            $out[] = $emoji . ' *' . $info->t . '* - ' . $info->l_cur . ' (' . $info->c . ')';
-                            if (isset($info->el_cur) && $info->el_cur) {
-                                $afterHours[] = '*' . $info->t . '* - ' . $info->el_cur;
-                            }
-                        }
-
-                        $out = implode(' | ', $out);
-                        if (count($afterHours)) {
-                            $out .= PHP_EOL . 'After hours: ' . implode(' | ', $afterHours);
-                        }
-                        $slack->respond($out);
-
-                    }
-                } else {
-                    echo 'no data';
+            // Execute everything
+            $active = null;
+            do {
+                $mrc = curl_multi_exec($mh, $active);
+            }  while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            while ($active && $mrc == CURLM_OK) {
+                if (curl_multi_select($mh) != -1) {
+                    do {
+                        $mrc = curl_multi_exec($mh, $active);
+                    } while ($mrc == CURLM_CALL_MULTI_PERFORM);
                 }
             }
 
+            $out = [];
+            foreach ($requests as $request) {
+                $out[] = self::_processStockRequest($mh, $request);
+            }
+
+            curl_multi_close($mh);
+
+            if (count($out) > 0) {
+                $slack->respond(implode(' | ', $out));
+            }
+
+        }
+
+        private static function _processStockRequest($mh, $request) {
+            $dataContent = json_decode(curl_multi_getcontent($request->dataRequest));
+            curl_multi_remove_handle($mh, $request->dataRequest);
+
+            return ($dataContent->Change > 0 ? ':green_heart:' : ':broken_heart:') . ' *' . $request->symbol . '* - ' . $dataContent->LastPrice . ' (' . round($dataContent->Change, 2) . ')';
+        }
+
+        private static function _createRequest($url) {
+            $c = curl_init();
+            curl_setopt($c, CURLOPT_URL, $url);
+            curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+            return $c;
+        }
+
+        /**
+         * Creates two requests. One to get current data, one to get historical data with which to determine
+         * the amount the stock is up/down currently
+         */
+        private static function _createStockRequest($symbol) {
+            return (object)[
+                'symbol' => $symbol,
+                'dataRequest' => self::_createRequest('http://dev.markitondemand.com/Api/v2/Quote/json?symbol=' . $symbol),
+            ];
         }
 
     }
